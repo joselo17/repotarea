5c9d989f985cb4134bf3d70ded2756333513ed66
joselo17 <120488979+joselo17@users.noreply.github.com>
2022-12-13 17:05:03 -0500

    resduen

--- codgio.py
+++ codgio.py
@@ -1,8 +1,29 @@
 import streamlit as st
-import mymodel as m
+
 st.write("""# PRUEBA MUNDO MI PODER""")
 st.write("""# texto para probar app""")
-window=st.slider("heloo desliza")
-st.write(m.run(window=window))
 
-install mymodel
+st.write("""Desde que Úrsula Iguarán se casó con su primo José Arcadio Buendía, teme engendrar un niño con cola de cerdo como consecuencia del parentesco. Por ello, se niega temporalmente a consumar el matrimonio. Esto es causa de que Prudencio Aguilar se burle de José Arcadio Buendía quien, ofendido, lo mata en duelo para salvar su honor. Desde entonces, el fantasma de Aguilar lo persigue y José Arcadio decide irse del pueblo.
+
+Inspirado por un sueño durante su travesía en la selva, José Arcadio Buendía decide quedarse en ese punto del camino y fundar Macondo, pueblo que crece poco a poco.
+
+El pueblo recibe con frecuencia las visitas de los gitanos. Su líder, Melquíades, trae siempre artefactos y objetos que obsesionan a José Arcadio Buendía.
+
+Para entonces, el joven matrimonio ya ha concebido a tres hijos: José Arcadio, Aureliano y Amaranta. Además, adoptan a Rebeca, la hija de unos parientes. El incesto es una preocupación constante en Úrsula, quien con los años observa cómo Receba y su hijo José Arcadio se enamoran y se casan.
+
+A Macondo llega la peste del insomnio, la cual trae consigo la del olvido. Un brebaje de Melquíades pone fin a la peste. El éxito es tal que el gitano se queda a vivir en Macondo, tiempo en el cual escribe unos pergaminos que solo serán descifrados muchos años depués.
+
+El patriarca, José Arcadio Buendía, vuelve a encontrarse con el fantasma de Aguilar y enloquece. La familia, entonces, lo amarra a un árbol del patio trasero, donde morirá de un infarto.
+
+II etapa: la guerra civil y el coronel Aureliano Buendía
+Al estallar la guerra civil, Aureliano Buendía lucha contra los conservadores, comandando un grupo de soldados de Macondo. Nombra a su sobrino Arcadio como jefe civil y militar del pueblo.
+
+Arcadio había sido el fruto de una relación de amantes entre José Arcado hijo y Pilar Ternera, regente de un prostíbulo. Fue criado en casa de sus abuelos a condición de que se le ocultara su origen. Creció pensando ser hijo del gran patriarca. Cuando es nombrado jefe de Macondo, Arcadio se vuelve un dictador y tiraniza el pueblo. Muere fusilado por los conservadores.
+
+Durante su actividad como líder de los liberales, el coronel Aureliano Buendía enfrenta un total de 32 batallas, de las cuales sale siempre perdedor. Cansado, pronto el coronel comprende que la lucha armada no tiene sentido.
+
+Con el tiempo, Aureliano firma un tratado de paz, tras el cual intenta suicidarse. Regresa a Macondo, donde pasará el resto de su vida haciendo y rehaciendo pescaditos de oro.
+
+Coronel Aureliano
+Marco Cabras: Ilustración del coronel Aureliano Buendía en su retiro militar, haciendo y rehaciendo pescaditos de oro.""")
+
