9533b20fc3892b7eb6eb7fbc3fc18e463788dc65
joselo17 <120488979+joselo17@users.noreply.github.com>
2022-12-16 16:42:05 -0500

    3r

--- menu.py
+++ menu.py
@@ -16,18 +16,18 @@ with st.sidebar:
                          icons=['house', 'camera fill', 'kanban', 'book','person lines fill'],
                          menu_icon="app-indicator", default_index=0,
                         
-if choose == "PRESENTACION":
+    if choose == "PRESENTACION":
     st.write("1ERA OPCION Sharone Li is a data science practitioner, enthusiast, and blogger. She writes data science articles and tutorials about Python, data visualization, Streamlit, etc. She is also an amateur violinist who loves classical music.\n\nTo read Sharone's data science posts, please visit her Medium blog at: https://medium.com/@insightsbees")    
     
-elif choose == "PROPUESTA":
+    elif choose == "PROPUESTA":
     st.write("2DA OPCION Sharone Li is a data science practitioner, enthusiast, and blogger. She writes data science articles and tutorials about Python, data visualization, Streamlit, etc. She is also an amateur violinist who loves classical music.\n\nTo read Sharone's data science posts, please visit her Medium blog at: https://medium.com/@insightsbees")    
      
-elif choose == "CUADRO DE CORRELACIOn":
+    elif choose == "CUADRO DE CORRELACIOn":
     st.write("3ERA OPCION Sharone Li is a data science practitioner, enthusiast, and blogger. She writes data science articles and tutorials about Python, data visualization, Streamlit, etc. She is also an amateur violinist who loves classical music.\n\nTo read Sharone's data science posts, please visit her Medium blog at: https://medium.com/@insightsbees")    
      
-elif choose == "PERSONAL":
+    elif choose == "PERSONAL":
     st.write("4TA OPCION Sharone Li is a data science practitioner, enthusiast, and blogger. She writes data science articles and tutorials about Python, data visualization, Streamlit, etc. She is also an amateur violinist who loves classical music.\n\nTo read Sharone's data science posts, please visit her Medium blog at: https://medium.com/@insightsbees")    
      
-elif choose == "CONCLUSIONES":
+    elif choose == "CONCLUSIONES":
     st.write("5TA OPCION Sharone Li is a data science practitioner, enthusiast, and blogger. She writes data science articles and tutorials about Python, data visualization, Streamlit, etc. She is also an amateur violinist who loves classical music.\n\nTo read Sharone's data science posts, please visit her Medium blog at: https://medium.com/@insightsbees")    
      
