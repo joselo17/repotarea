9c6cdc7f15285f790316c9d0961266a04365d4a0
joselo17 <120488979+joselo17@users.noreply.github.com>
2022-12-17 19:35:14 -0500

    ultima prueba

--- menu.py
+++ menu.py
@@ -5,9 +5,7 @@ from streamlit_option_menu import option_menu
 import streamlit.components.v1 as html
 from  PIL import Image
 import numpy as np
-import cv2
 import pandas as pd
-from st_aggrid import AgGrid
 import plotly.express as px
 import io 
 
@@ -16,18 +14,18 @@ with st.sidebar:
                          icons=['house', 'camera fill', 'kanban', 'book','person lines fill'],
                          menu_icon="app-indicator", default_index=0,
                         
-    if choose == "PRESENTACION":
+if choose == "PRESENTACION":
     st.write("1ERA OPCION Sharone Li is a data science practitioner, enthusiast, and blogger. She writes data science articles and tutorials about Python, data visualization, Streamlit, etc. She is also an amateur violinist who loves classical music.\n\nTo read Sharone's data science posts, please visit her Medium blog at: https://medium.com/@insightsbees")    
     
-    elif choose == "PROPUESTA":
+elif choose == "PROPUESTA":
     st.write("2DA OPCION Sharone Li is a data science practitioner, enthusiast, and blogger. She writes data science articles and tutorials about Python, data visualization, Streamlit, etc. She is also an amateur violinist who loves classical music.\n\nTo read Sharone's data science posts, please visit her Medium blog at: https://medium.com/@insightsbees")    
      
-    elif choose == "CUADRO DE CORRELACIOn":
+elif choose == "CUADRO DE CORRELACIOn":
     st.write("3ERA OPCION Sharone Li is a data science practitioner, enthusiast, and blogger. She writes data science articles and tutorials about Python, data visualization, Streamlit, etc. She is also an amateur violinist who loves classical music.\n\nTo read Sharone's data science posts, please visit her Medium blog at: https://medium.com/@insightsbees")    
      
-    elif choose == "PERSONAL":
+elif choose == "PERSONAL":
     st.write("4TA OPCION Sharone Li is a data science practitioner, enthusiast, and blogger. She writes data science articles and tutorials about Python, data visualization, Streamlit, etc. She is also an amateur violinist who loves classical music.\n\nTo read Sharone's data science posts, please visit her Medium blog at: https://medium.com/@insightsbees")    
      
-    elif choose == "CONCLUSIONES":
+elif choose == "CONCLUSIONES":
     st.write("5TA OPCION Sharone Li is a data science practitioner, enthusiast, and blogger. She writes data science articles and tutorials about Python, data visualization, Streamlit, etc. She is also an amateur violinist who loves classical music.\n\nTo read Sharone's data science posts, please visit her Medium blog at: https://medium.com/@insightsbees")    
      
